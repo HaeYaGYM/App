e59a4b3bae1d9d0617bcdfc75a326c2fec3c0fe8
superman2080 <superman2080@naver.com>
2022-06-01 16:22:43 +0900

    Function to add routine 2

---
+++ app/src/main/java/com/example/haeyagym/DBHelper.java
@@ -0,0 +1,47 @@
+package com.example.haeyagym;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+interface DBContract {
+    static final String TABLE_NAME="ROUTINE";
+    static final String ROU_ID="ID";
+    static final String ROU_NAME="NAME";
+    static final String ROU_SET_COUNT="COUNT";
+    static final String ROU_EXER_TIME="EXER";
+    static final String ROU_BREAK="BREAK";
+
+    static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " +
+            TABLE_NAME + "(" +
+            ROU_ID + " INTEGER NOT NULL PRIMARY KEY," +
+            ROU_NAME + " TEXT NOT NULL, " +
+            ROU_SET_COUNT + " TEXT NOT NULL, " +
+            ROU_EXER_TIME + " TEXT NOT NULL, " +
+            ROU_BREAK + " TEXT NOT NULL)";
+    static final String SQL_DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    static final String SQL_LOAD = "SELECT * FROM " + TABLE_NAME;
+    static final String SQL_SELECT = "SELECT * FROM "  + TABLE_NAME + " WHERE " + ROU_NAME + "=?";
+    static final String SQL_SELECT_ID = "SELECT ID FROM "  + TABLE_NAME + " WHERE " + ROU_NAME + "=? and " + ROU_EXER_TIME + "=? and" + ROU_BREAK + "=?";
+    static final String SQL_UPDATE_ID = "ALTER TABLE " + TABLE_NAME + " AUTO_INCREAMENT=1 SET @COUNT = 0 UPDATE " + TABLE_NAME + "." + ROU_ID + " = @COUNT:=COUNT+1";
+}
+
+class DBHelper extends SQLiteOpenHelper {
+    static final String DB_FILE = "routines.db";
+    static final int DB_VERSION = 1;
+
+    DBHelper(Context context) {
+        super(context, DB_FILE, null, DB_VERSION); // 세번째 인자 : cursor factory
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DBContract.SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DBContract.SQL_DROP_TABLE);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/haeyagym/TimerActivity.java
+++ app/src/main/java/com/example/haeyagym/TimerActivity.java
@@ -7,9 +7,13 @@ import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -25,7 +29,11 @@ public class TimerActivity extends AppCompatActivity {
     private int exerciseMin = 3, exerciseSec = 0;
     private int breakMin = 0, breakSec = 30;
 
-    private int routineCount;           //루틴 인덱스값
+    private int maxRoutineListCount;
+
+    private DBHelper dbHelper;
+    SQLiteDatabase db;
+
     private TextView textCount;
     private TextView textExer;
     private TextView textBreak;
@@ -38,23 +46,52 @@ public class TimerActivity extends AppCompatActivity {
     private ActivityResultCallback<ActivityResult> resultCallback;
     private ActivityResultLauncher<Intent> routineLauncher;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        //============================================================================== 초기화 부분
         textCount = findViewById(R.id.textSetCount);
         textExer = findViewById(R.id.textTimerCount);
         textBreak = findViewById(R.id.textBreakCount);
         routineList = findViewById(R.id.routineList);
+
+        dbHelper = new DBHelper(this);
         routineListData = new ArrayList<>();
 
-
-        routineListData.clear();
         simpleAdapter = new SimpleAdapter(this, routineListData, R.layout.exercise_item, new String[] {"exerName", "setCount", "exerTime", "breakTime"},
                 new int[] {R.id.textItemExerciseName, R.id.textItemSetCount, R.id.textItemExerciseTime, R.id.textItemBreakTime});
+
+        routineListData.clear();
         routineList.setAdapter(simpleAdapter);
+
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(DBContract.SQL_LOAD, null);
+        int i = 0;
+        while(cursor.moveToNext()) {
+            HashMap<String, String> temp = new HashMap<>();
+
+            temp.put("id", cursor.getString(0));
+            temp.put("exerName", cursor.getString(1));
+            temp.put("setCount", cursor.getString(2));
+            temp.put("exerTime", cursor.getString(3));
+            temp.put("breakTime", cursor.getString(4));
+            routineListData.add(temp);
+            maxRoutineListCount = Math.max(maxRoutineListCount, Integer.parseInt(routineListData.get(i).get("id")));
+            i++;
+        }
+
+        cursor.close();
+
+        simpleAdapter.notifyDataSetChanged();
+
+        //============================================================================== 초기화 부분
+
+
+
+
+
 
         resultContract = new ActivityResultContracts.StartActivityForResult();
         resultCallback = new ActivityResultCallback<ActivityResult>() {             //콜백객체 초기화, 리절트 이벤트 발생 시 루틴 추가해줌
@@ -63,11 +100,24 @@ public class TimerActivity extends AppCompatActivity {
                 if(result.getResultCode() == RESULT_OK){
                     Intent intent = result.getData();
                     HashMap<String, String> temp = new HashMap<>();
+                    maxRoutineListCount++;
+
+                    temp.put("id", String.valueOf(maxRoutineListCount));
                     temp.put("exerName", intent.getStringExtra("exerName"));
                     temp.put("setCount", intent.getStringExtra("setCount"));
                     temp.put("exerTime", intent.getStringExtra("exerTime"));
                     temp.put("breakTime", intent.getStringExtra("breakTime"));
+
+                    ContentValues values = new ContentValues();
+                    values.put("ID", String.valueOf(maxRoutineListCount));
+                    values.put("NAME", intent.getStringExtra("exerName"));
+                    values.put("COUNT", intent.getStringExtra("setCount"));
+                    values.put("EXER", intent.getStringExtra("exerTime"));
+                    values.put("BREAK", intent.getStringExtra("breakTime"));
                     routineListData.add(temp);
+
+                    db = dbHelper.getWritableDatabase();
+                    db.insert(DBContract.TABLE_NAME, null, values);
                     simpleAdapter.notifyDataSetChanged();
                 }
                 else
@@ -77,6 +127,16 @@ public class TimerActivity extends AppCompatActivity {
         routineLauncher = registerForActivityResult(resultContract, resultCallback);
         //==============================================================================
 
+        routineList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+               Toast.makeText(getApplicationContext(), routineListData.get(i).get("id"), Toast.LENGTH_LONG).show();
+               db.delete(DBContract.TABLE_NAME, "ID=?", new String[] {routineListData.get(i).get("id")});
+               routineListData.remove(i);
+               simpleAdapter.notifyDataSetChanged();
+
+           }
+        });
 
     }
 
