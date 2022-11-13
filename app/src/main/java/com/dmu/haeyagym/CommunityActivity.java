627da8c8c505e99eb6c734beae0fca9d7d1a22e9
superman2080 <superman2080@naver.com>
2022-11-14 03:09:15 +0900

    # Conflicts: #	.idea/gradle.xml #	.idea/misc.xml #	app/build.gradle #	app/src/main/AndroidManifest.xml #	app/src/main/res/values-night/themes.xml #	app/src/main/res/values/strings.xml #	app/src/main/res/values/themes.xml #	build.gradle #	gradle.properties #	gradle/wrapper/gradle-wrapper.jar #	gradle/wrapper/gradle-wrapper.properties #	gradlew #	gradlew.bat #	settings.gradle
    Community Upload Complete

--- app/src/main/java/com/dmu/haeyagym/CommunityActivity.java
+++ app/src/main/java/com/dmu/haeyagym/CommunityActivity.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -32,6 +32,7 @@ public class CommunityActivity extends AppCompatActivity {
     private BottomNavigationView bottomNav;
     private FirebaseDatabase firebaseDB;
     private DatabaseReference dbRef;
+    private Thread listViewRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,27 +53,9 @@ public class CommunityActivity extends AppCompatActivity {
         simpleAdapter = new SimpleAdapter(this, listViewGroupData, R.layout.group_item,
                 new String[] {"textTitle", "textCategory"},
                 new int[] {R.id.textTitle, R.id.textCategory});
-
         listViewGroupData.clear();
         listViewGroup.setAdapter(simpleAdapter);
-
-        listViewGroupData = GetGroupList();
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < listViewGroupData.size(); i++){
-                    for (String str : listViewGroupData.get(i).values()){
-                        test.setText(str);
-                    }
-                }
-            }
-        });
-        thread.start();
         //
-        
-        listViewGroup.setAdapter(simpleAdapter);
-        simpleAdapter.notifyDataSetChanged();
 
         //바텀 네비게이션
         bottomNav.setSelectedItemId(R.id.item_frag4);
@@ -106,39 +89,51 @@ public class CommunityActivity extends AppCompatActivity {
             }
         });
         //
-    }
 
-    private ArrayList<HashMap<String, String>> GetGroupList() {
-        firebaseDB = FirebaseDatabase.getInstance();
-        dbRef = firebaseDB.getReference("board");
-
-
-        ArrayList<HashMap<String, String>> temp = new ArrayList<>();
-
-        dbRef.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+        //리스트 가져오기
+        GetList();
+        //
+        listViewRefresh = new Thread(new Runnable() {
             @Override
-            public void onSuccess(DataSnapshot dataSnapshot) {
-                if(dataSnapshot != null){
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                        HashMap<String, String> datas = (HashMap<String, String>)snapshot.getValue();
-                        HashMap<String, String> temp = new HashMap<>();
-                        temp.put("imagePictogram", datas.get("Category"));
-                        temp.put("textTitle", datas.get("Title"));
-                        temp.put("textGroupName", datas.get("Category"));
-                        Log.d("Test",temp.toString());
-                        listViewGroupData.add(temp);
+            public void run() {
+                while (true){
+                    if(listViewGroupData.size() > 0) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                simpleAdapter.notifyDataSetChanged();
+                            }
+                        });
                     }
-                    simpleAdapter.notifyDataSetChanged();
-//                    for (int i = 0; i < temp.size(); i++){
-//                        for (String str : temp.get(i).values()){
-//                            Log.d("Taew", str);
-//                        }
-//                    }
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception e){e.printStackTrace();}
                 }
             }
         });
+        listViewRefresh.start();
+        //
+    }
 
-        return temp;
+    private void GetList(){
+        firebaseDB = FirebaseDatabase.getInstance();
+        dbRef = firebaseDB.getReference("board");
+
+        dbRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    DataSnapshot result = task.getResult();
+                    for (DataSnapshot data : result.getChildren()){
+                        HashMap<String, String> temp = (HashMap<String, String>)data.getValue();
+                        temp.put("textTitle", temp.get("Title"));
+                        temp.put("textCategory", temp.get("Category"));
+                        listViewGroupData.add(temp);
+                    }
+                }
+            }
+        });
     }
 
     public void AddGroup(View view) {
